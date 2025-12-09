3c890b42d4c7f3bd5ca33f20445bf2049c56a314
Daniela-Padilha <danielasofiapadilha@gmail.com>
2025-12-09 19:38:14 +0000

    ex1 completed

---
+++ PY00/ft_plot_area.py
@@ -0,0 +1,12 @@
+
+def ft_plot_area():
+    length: int = 0
+    width: int = 0
+    res: int = 0
+
+    print("Enter length: ", end="")
+    length = int(input())
+    print("Enter width: ", end="")
+    width = int(input())
+    res = length * width
+    print("Plot area:", res)
